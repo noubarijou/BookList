af536469691a6a287b53f4eba517ac0e66492624
noubarijou <85699669+noubarijou@users.noreply.github.com>
2021-10-02 21:56:16 -0300

    adds persistence to localStorage

--- js/appES6.js
+++ js/appES6.js
@@ -41,6 +41,44 @@ class UI {
   }
 }
 
+// local storage class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else {
+            books = JSON.parse(localStorage.getItem('books'));;
+        }
+        return books;
+    }
+    static displayBooks(){
+        const books = Store.getBooks();
+        books.forEach(function(book){
+            const ui = new UI;
+            ui.addBookToList(book);
+        });
+
+    }
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+            
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+
 document.getElementById("book-form").addEventListener("submit", (e) => {
   const title = document.getElementById("title").value,
     author = document.getElementById("author").value,
@@ -53,6 +91,7 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
     ui.showAlert("Please fill in all fields", "error");
   } else {
     ui.addBookToList(book);
+    Store.addBook(book);
     ui.showAlert("Book added!", "success");
     ui.clearFields();
   }
@@ -62,6 +101,7 @@ document.getElementById("book-form").addEventListener("submit", (e) => {
 document.getElementById("book-list").addEventListener("click", (e) => {
   const ui = new UI();
   ui.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   ui.showAlert("Book Removed", "success");
   e.preventDefault();
 });
